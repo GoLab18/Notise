f98e254ed2c6a6cfc4c2cd7c388204f32e811b99
GoLab18 <kuba1golab@gmail.com>
2024-07-28 23:18:11 +0200

    Added a window for adding notes to folders

---
+++ lib/components/add_to_folder_window.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/components/button_template.dart';
+import 'package:notes_app/data/folder.dart';
+import 'package:notes_app/data/notes_database.dart';
+import 'package:provider/provider.dart';
+
+class AddToFolderWindow extends StatefulWidget {
+  final int noteId;
+  final List<Folder> currentFolders;
+  final void Function()? popPreviusWindow;
+
+  const AddToFolderWindow({
+    super.key,
+    required this.noteId,
+    required this.currentFolders,
+    this.popPreviusWindow
+  });
+
+  @override
+  State<AddToFolderWindow> createState() => _AddToFolderWindowState();
+}
+
+class _AddToFolderWindowState extends State<AddToFolderWindow> {
+  int? _selectedFolderId;
+
+  bool isErrorVisible = false;
+
+
+  void addOrShowError() {
+    if (_selectedFolderId != null) {
+
+        // Is note inside the specified folder
+        bool isInsideFolder = (
+          widget.currentFolders.firstWhere((folder) => folder.id == _selectedFolderId).notesIds.isNotEmpty
+          && widget.currentFolders.firstWhere((folder) => folder.id == _selectedFolderId).notesIds.contains(widget.noteId)
+        );
+
+        if (isInsideFolder) {
+          setState(() {
+            isErrorVisible = true;
+          });
+        } else {
+          context.read<NotesDatabase>().addNoteToFolder(
+            widget.noteId,
+            _selectedFolderId!,
+            null
+          );
+
+          Navigator.pop(context);
+          if (widget.popPreviusWindow != null) widget.popPreviusWindow!();
+        }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: const Color.fromARGB(255, 9, 61, 140),
+      content: Container(
+        width: 200,
+        height: 180,
+        decoration:  const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+
+            // Action title
+            const Text(
+              "Add to folder..",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20
+              ),
+            ),
+
+            // Folders available
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+              child: Column(
+                children: [
+                  DropdownMenu<Folder>(
+                    onSelected: (Folder? value) {
+                      setState(() {
+                        if (value != null) {
+                          if (isErrorVisible && _selectedFolderId != value.id) {
+                              isErrorVisible = false;
+                          }
+
+                          _selectedFolderId = value.id;
+                        }
+                      });
+                    },
+                    expandedInsets: EdgeInsets.zero,
+                    leadingIcon: const Icon(
+                      Icons.folder,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    trailingIcon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.white,
+                    ),
+                    selectedTrailingIcon: const Icon(
+                      Icons.arrow_drop_up,
+                      color: Colors.white
+                    ),
+                    textStyle: const TextStyle(color: Colors.white),
+                    menuStyle: const MenuStyle(
+                      surfaceTintColor: WidgetStatePropertyAll(Color.fromARGB(255, 44, 4, 67)),
+                      backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 9, 61, 140))
+                    ),
+                    dropdownMenuEntries: widget.currentFolders.map<DropdownMenuEntry<Folder>>((Folder folder) {
+                      return DropdownMenuEntry<Folder>(
+                        value: folder,
+                        label: folder.name,
+                        labelWidget: Text(
+                          folder.name,
+                          style: const TextStyle(
+                            color: Colors.white
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis
+                        )
+                      );
+                    }).toList()
+                  ),
+
+                  // Error text
+                  SizedBox(
+                    height: 20,
+                    child: Visibility(
+                      visible: isErrorVisible,
+                      child: Text(
+                        "Note is already inside the folder!",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.red.shade800
+                        )
+                      )
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            // Buttons row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ButtonTemplate(
+                  text: "Add",
+                  onPressed: addOrShowError
+                ),
+                ButtonTemplate(
+                  text: "Cancel",
+                  onPressed: () {
+                  Navigator.pop(context);
+                  if (widget.popPreviusWindow != null) widget.popPreviusWindow!();
+                })
+              ]
+            )
+          ]
+        )
+      )
+    );
+  }
+}
