8c6706f9d251f126cc38fcdea0b0b6a4bcd7d661
GoLab18 <kuba1golab@gmail.com>
2024-07-29 15:21:01 +0200

    Folder page initialization

---
+++ lib/pages/folder_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/data/folder.dart';
+
+class FolderPage extends StatefulWidget {
+  final Folder folder;
+
+  const FolderPage({
+    super.key,
+    required this.folder
+  });
+
+  @override
+  State<FolderPage> createState() => _FolderPageState();
+}
+
+class _FolderPageState extends State<FolderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Text("Folder page")
+    );
+  }
+}
