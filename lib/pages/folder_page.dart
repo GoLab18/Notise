7698c75d1caf64fc86faef421703b6aa12dfbf21
GoLab18 <kuba1golab@gmail.com>
2024-08-03 19:03:18 +0200

    Added title and folder notes viewing and changing
    Added MainAppBar for folder interactions, CustomFloatingActionButton for adding folder notes and note tiles animated interactions

--- lib/pages/folder_page.dart
+++ lib/pages/folder_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:notise/components/add_note_window.dart';
+import 'package:notise/components/custom_floating_action_button.dart';
+import 'package:notise/components/main_app_bar.dart';
 import 'package:notise/data/folder.dart';
+import 'package:notise/data/notes_database.dart';
+import 'package:notise/pages/notes_view_page.dart';
+import 'package:notise/util/date_util.dart';
+import 'package:provider/provider.dart';
 
 class FolderPage extends StatefulWidget {
   final Folder folder;
@@ -14,11 +21,199 @@ class FolderPage extends StatefulWidget {
 }
 
 class _FolderPageState extends State<FolderPage> {
+  late TextEditingController _folderNameController;
+  late FocusNode _folderNameFocusNode;
+  late bool isInEditState;
+
+  // For disabling UI parts on bottom sheet opening
+  late bool _isBottomSheetOpen;
+
+  late TextEditingController titleController;
+  late TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _folderNameController = TextEditingController(text: widget.folder.name);
+    _folderNameFocusNode = FocusNode();
+    isInEditState = false;
+    _isBottomSheetOpen = false;
+    _folderNameFocusNode.addListener(handleFocusChange);
+
+    titleController = TextEditingController();
+    textController = TextEditingController();
+  }
+
+  void handleFocusChange() {
+    setState(() {
+      isInEditState = _folderNameFocusNode.hasFocus;
+    });
+  }
+
+  void onEditStateFinished() {
+    setState(() {
+      _folderNameFocusNode.unfocus();
+
+      updateFolder(
+        widget.folder.id,
+        _folderNameController.text
+      );
+
+      isInEditState = false;
+    });
+  }
+
+  void updateFolder(int id, String newFolderName) {
+    context.read<NotesDatabase>().updateFolder(id, newFolderName);
+  }
+
+  void deleteFolder(int id) {
+    context.read<NotesDatabase>().deleteFolder(id);
+  }
+
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AddNoteWindow(
+          titleController: titleController,
+          textController: textController,
+          onAddPressed:  () {
+            context.read<NotesDatabase>().addNote(titleController.text, textController.text, widget.folder.id);
+
+            titleController.clear();
+            textController.clear();
+
+            Navigator.pop(context);
+          },
+          onCancelPressed: () {
+            titleController.clear();
+            textController.clear();
+
+            Navigator.pop(context);
+          }
+        )
+    );
+  }
+  
+  @override
+  void dispose() {
+    _folderNameController.dispose();
+    _folderNameFocusNode.removeListener(handleFocusChange);
+    _folderNameFocusNode.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final NotesDatabase db = context.watch<NotesDatabase>();
+
+    Map<int, int> currentFoldersNotesAmounts = db.currentFoldersNotesAmounts;
+
     return Scaffold(
-      appBar: AppBar(),
-      body: const Text("Folder page")
+      backgroundColor: Colors.blue.shade800,
+      appBar: MainAppBar(
+        showBackButton: true,
+        isInEditState: isInEditState,
+        onEditStateFinished: onEditStateFinished,
+        popupMenuOptionsCallbacks: {
+          "Delete": () {
+            deleteFolder(widget.folder.id);
+          }
+        },
+      ),
+      body: Column(
+        children: [
+
+          // Note title
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                // Editable folder name
+                EditableText(
+                  keyboardType: TextInputType.multiline,
+                  controller: _folderNameController,
+                  focusNode: _folderNameFocusNode,
+                  style: const TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    overflow: TextOverflow.ellipsis
+                  ),
+                  cursorColor: Colors.blue,
+                  backgroundCursorColor: Colors.grey
+                ),
+                
+                Row(
+                  children: [
+
+                    // Init date
+                    Text(
+                      DateUtil.getCurrentDate(widget.folder.initDate),
+                      style: const TextStyle(
+                        color: Colors.white54,
+                        fontSize: 14
+                      )
+                    ),
+
+                    // Notes
+                    const Padding(
+                      padding: EdgeInsets.only(left: 14, right: 6),
+                      child: Icon(
+                        Icons.note_outlined,
+                        color: Colors.white54,
+                        size: 16,
+                      )
+                    ),
+        
+                    // Amount of notes inside
+                    Text(
+                      currentFoldersNotesAmounts[widget.folder.id].toString(),
+                      style: const TextStyle(
+                        color: Colors.white54,
+                        fontSize: 14
+                      ),
+                      textAlign: TextAlign.center
+                    )
+                  ]
+                )
+              ],
+            ),
+          ),
+
+          const Divider(
+            color: Colors.white,
+            thickness: 2,
+            height: 0
+          ),
+
+          // List for notes
+          Expanded(
+            child: NotesViewPage(
+              onBottomSheetOpened: () {
+                setState(() {
+                  _isBottomSheetOpen = true;
+                });
+              },
+              onBottomSheetClosed: () {
+                setState(() {
+                  _isBottomSheetOpen = false;
+                });
+              },
+              viewSpecificFolderNotes: true,
+              folderId: widget.folder.id
+            )
+          )
+        ]
+      ),
+      floatingActionButton: Visibility(
+        visible: !_isBottomSheetOpen,
+        child: CustomFloatingActionButton(
+          onPressed: createNote
+        )
+      ),
     );
   }
 }
