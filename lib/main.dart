149ac6c22adbca2aadadb69f1edb1c3f2c25c9a4
GoLab18 <kuba1golab@gmail.com>
2024-08-07 18:55:33 +0200

    Added theme shared preferences initialization before app start

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotesDatabase.initialization();
+  await ThemeProvider.loadThemePreference();
 
   runApp(
     MultiProvider(
