80a80f1a0ee6c47b8e3e6fdc1030e6087be0840a
GoLab18 <kuba1golab@gmail.com>
2024-08-07 19:02:32 +0200

    Added shared preferences caching for user's theme choice

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,26 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:notise/themes/themes.dart';
 
 class ThemeProvider with ChangeNotifier {
-  ThemeData _themeData = lightMode;
+  static const String _themePrefKey = 'themePref';
+  
+  static ThemeData _themeData = lightMode;
 
   ThemeData get themeData => _themeData;
 
   set themeData(ThemeData themeData) {
     _themeData = themeData;
-
+    _saveThemePreference(themeData == darkMode);
     notifyListeners();
   }
 
   bool get isDarkMode => _themeData == darkMode;
-  
 
-// Method for user darkMode toggle
+
+  // Method for user darkMode toggle
   void toggleTheme() {
-    if (_themeData == lightMode) {
-      themeData = darkMode;
-    } else {
-      themeData = lightMode;
+    themeData = (_themeData == lightMode) ? darkMode : lightMode;
+  }
+
+  // Load the theme preference
+  static Future<void> loadThemePreference() async {
+    final SharedPreferencesWithCache prefsWithCache = await SharedPreferencesWithCache.create(
+      cacheOptions: const SharedPreferencesWithCacheOptions(
+        allowList: <String>{_themePrefKey}
+      ),
+    );
+
+    final bool? isDarkMode = prefsWithCache.getBool(_themePrefKey);
+
+    if (isDarkMode != null) {
+      _themeData = isDarkMode ? darkMode : lightMode;
     }
+  }
+
+  // Save the theme preference
+  Future<void> _saveThemePreference(bool isDarkMode) async {
+    final SharedPreferencesWithCache prefsWithCache = await SharedPreferencesWithCache.create(
+      cacheOptions: const SharedPreferencesWithCacheOptions(
+        allowList: <String>{_themePrefKey}
+      ),
+    );
+
+    await prefsWithCache.setBool(_themePrefKey, isDarkMode);
   }
 }
