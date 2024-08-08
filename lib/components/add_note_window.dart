80510e979e24d7ac6211629c6c52928d501b29d1
GoLab18 <kuba1golab@gmail.com>
2024-08-08 19:39:45 +0200

    Wrapped in a SingleChildScrollView widget

--- lib/components/add_note_window.dart
+++ lib/components/add_note_window.dart
@@ -20,73 +20,39 @@ class AddNoteWindow extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
 
-    return AlertDialog(
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      content: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxHeight: screenSize.height * 0.5,
-          maxWidth: screenSize.width * 0.7
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            
-            // Data column
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  
-                  // Action title
-                  Text(
-                    "New note",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                      fontSize: 20
-                    ),
-                  ),
-        
-                  // Title
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8, bottom: 6),
-                    child: TextField(
-                      controller: titleController,
+    return SingleChildScrollView(
+      child: AlertDialog(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        content: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: screenSize.height * 0.5,
+            maxWidth: screenSize.width * 0.7
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              
+              // Data column
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    
+                    // Action title
+                    Text(
+                      "New note",
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontSize: 20
                       ),
-                      decoration: InputDecoration(
-                        fillColor: Theme.of(context).colorScheme.surface,
-                        filled: true,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: const BorderRadius.all(Radius.circular(8)),
-                          borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.inversePrimary
-                            )
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: const BorderRadius.all(Radius.circular(8)),
-                          borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.inversePrimary
-                            )
-                        ),
-                        hintText: "Title..",
-                        hintStyle: TextStyle(
-                          color: Theme.of(context).colorScheme.tertiary
-                        )
-                      )
                     ),
-                  ),
-                  
-                  // Text
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
+          
+                    // Title
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 6),
                       child: TextField(
-                        controller: textController,
-                        maxLines: null,
-                        expands: true,
-                        keyboardType: TextInputType.multiline,
+                        controller: titleController,
                         style: TextStyle(
                           color: Theme.of(context).colorScheme.inversePrimary
                         ),
@@ -96,33 +62,69 @@ class AddNoteWindow extends StatelessWidget {
                           enabledBorder: OutlineInputBorder(
                             borderRadius: const BorderRadius.all(Radius.circular(8)),
                             borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.inversePrimary
-                            )
+                                color: Theme.of(context).colorScheme.inversePrimary
+                              )
                           ),
-                          hintText: "Note..",
+                          border: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(Radius.circular(8)),
+                            borderSide: BorderSide(
+                                color: Theme.of(context).colorScheme.inversePrimary
+                              )
+                          ),
+                          hintText: "Title..",
                           hintStyle: TextStyle(
                             color: Theme.of(context).colorScheme.tertiary
-                          ),
-                          alignLabelWithHint: true,
-                          hintTextDirection: TextDirection.ltr,
-                          floatingLabelAlignment: FloatingLabelAlignment.start
+                          )
                         )
-                      )
+                      ),
                     ),
-                  ),
-                ]
+                    
+                    // Text
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
+                        child: TextField(
+                          controller: textController,
+                          maxLines: null,
+                          expands: true,
+                          keyboardType: TextInputType.multiline,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary
+                          ),
+                          decoration: InputDecoration(
+                            fillColor: Theme.of(context).colorScheme.surface,
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(Radius.circular(8)),
+                              borderSide: BorderSide(
+                                color: Theme.of(context).colorScheme.inversePrimary
+                              )
+                            ),
+                            hintText: "Note..",
+                            hintStyle: TextStyle(
+                              color: Theme.of(context).colorScheme.tertiary
+                            ),
+                            alignLabelWithHint: true,
+                            hintTextDirection: TextDirection.ltr,
+                            floatingLabelAlignment: FloatingLabelAlignment.start
+                          )
+                        )
+                      ),
+                    ),
+                  ]
+                ),
               ),
-            ),
-
-            // Buttons row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ButtonTemplate(text: "Create", onPressed: onAddPressed),
-                ButtonTemplate(text: "Cancel", onPressed: onCancelPressed)
-              ]
-            )
-          ]
+      
+              // Buttons row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ButtonTemplate(text: "Create", onPressed: onAddPressed),
+                  ButtonTemplate(text: "Cancel", onPressed: onCancelPressed)
+                ]
+              )
+            ]
+          )
         )
       )
     );
